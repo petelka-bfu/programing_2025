56106772d0fb193a44690a58ee7465067819d8bd
petelka-bfu <maranandaniel2@gmail.com>
2025-09-30 19:10:18 +0200

    Add files via upload

---
+++ laba2/laba2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <bitset>
+#include <string>
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	short a;
+	std::cout << "Введите А: ";
+	std::cin >> a;
+
+	short i;
+	std::cout << "Введите номер бита: ";
+	std::cin >> i;
+
+	if (0 <= i && i <= 8) {
+		std::bitset<8> bit(a);
+		if (bit[i] == 1) {
+			int b;
+			int c;
+			std
+		}
+
+	}
+	else
+	{
+		std::cout << "Номер бита введён неправильно";
+	}
+}
